7648d978494f9c01c694b8502a017cb81cc7d29d
EorlEli <51798919+EorlEli@users.noreply.github.com>
2025-05-22 11:44:27 +0200

    Add ChatOpenAI integration and update dependencies for improved functionality

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 import asyncio
 import os
+
 from dotenv import load_dotenv
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain_openai import ChatOpenAI
+from langgraph.prebuilt import create_react_agent
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
 
 load_dotenv()
-print(os.getenv("OPENAI_API_KEY"))
+
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+
+stdio_server_params = StdioServerParameters(
+    command="python",
+    args=["C:\\Users\\46760\\mcp-crash-course\\servers\\math_server.py"],
+    env={"OPENAI_API_KEY": os.getenv("OPENAI_API_KEY")},
+)
+
+
+
+
 
 async def main():
     print("Hello from mcp-crash-course!") 
